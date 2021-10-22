79e858305564c9042f793a57b84188a06aad4ecc
Fajar PC <fajar.rachmat.h@gmail.com>
2021-10-22 07:27:11 +0700

    Register

--- _log-sign/register_exc.php
+++ _log-sign/register_exc.php
@@ -2,7 +2,7 @@
     $id_institusi=$_POST['id_institusi'];
     $id_mou=$_POST['id_mou'];
     $nama=$_POST['nama'];
-    $no_kontak=$_POST['no_kontak'];s
+    $no_kontak=$_POST['no_kontak'];
     $email=$_POST['email'];
     $password=$_POST['password'];
     $ulangi_password=$_POST['ulangi_password'];
@@ -18,8 +18,9 @@
     }
     else
     {
-        $sql_insert="INSERT INTO `tb_user` (`id_user`,`username_user`,`password_user`,`name_user`,`email_user`) 
-                        VALUES (NULL, $)";
+        if()
+        $sql_insert="INSERT INTO `tb_user` (`id_user`,`username_user`,`password_user`,`name_user`,`email_user`,`level_user`,`create_user`,`status_user`) 
+                        VALUES (NULL, $institusi, $password, $nama, $email, 2, date('Y-m-d'), 'Y')";
         $conn->query($sql_insert);
         ?>
         <script>			
