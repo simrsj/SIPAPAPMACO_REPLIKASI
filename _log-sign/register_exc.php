b2d2c07d1497bc82af2eea90490de49c275b6112
Fajar Rachmat H <fajar.rachmat.h@gmail.com>
2021-11-09 17:09:21 +0700

    Daftar

--- _log-sign/register_exc.php
+++ _log-sign/register_exc.php
@@ -1,48 +1,43 @@
 <?PHP
-    //$id_institusi=$_POST['id_institusi'];
-    $id_mou=$_POST['id_mou'];
-    $nama=$_POST['nama'];
-    $no_kontak=$_POST['no_kontak'];
-    $email=$_POST['email'];
-    $password=$_POST['password'];
-    $ulangi_password=$_POST['ulangi_password'];
-    $tanggal_sekarang=date('Y-m-d');
+//$id_institusi=$_POST['id_institusi'];
+$id_mou = $_POST['id_mou'];
+$nama = $_POST['nama'];
+$no_kontak = $_POST['no_kontak'];
+$email = $_POST['email'];
+$password = $_POST['password'];
+$ulangi_password = $_POST['ulangi_password'];
+$tanggal_sekarang = date('Y-m-d');
 
-    if($password!=$ulangi_password)
-    {
-        ?>
-            <script>
-                alert('Password tidak sesuai!');
-                document.location.href="?reg";
-            </script>
-        <?php
-    }
-    else
-    {
-        if($id_mou==0)
-        {
-            $nama_ip=$_POST['namMa_ip'];
-            $sql_mou="SELECT id_mou FROM tb_mou order by id_mou ASC";
-            
-            $q_mou=$conn->query($sql_mou);
-            $r_mou = $q_mou->rowCount();
-            while ($d_mou = $q_mou->fetch(PDO::FETCH_ASSOC))
-            {
-                $id_mou_baru=$d_mou['id_mou']+1;
-            }
-            $id_mou=$id_mou_baru;
-            
-            $sql_insert_mou="INSERT INTO `tb_mou` (`id_mou`,`institute_mou`,`contact_mou`) 
-                                VALUES ($id_mou, $nama_ip, $no_kontak)";
-            $conn->query($sql_insert_mou);    
+if ($password != $ulangi_password) {
+?>
+    <script>
+        alert('Password tidak sesuai!');
+        document.location.href = "?reg";
+    </script>
+<?php
+} else {
+    if ($id_mou == 0) {
+        $nama_ip = $_POST['nama_ip'];
+        $sql_mou = "SELECT id_mou FROM tb_mou order by id_mou ASC";
+
+        $q_mou = $conn->query($sql_mou);
+        $r_mou = $q_mou->rowCount();
+        while ($d_mou = $q_mou->fetch(PDO::FETCH_ASSOC)) {
+            $id_mou_baru = $d_mou['id_mou'] + 1;
         }
-        $sql_insert_user="INSERT INTO tb_user (id_user, username_user, password_user, name_user, email_user, level_user, create_user, id_mou, status_user) 
-                        VALUES (NULL, '$email', '$password', '$nama', '$email', '2', '$tanggal_sekarang', '$id_mou', 'Y')";
-        $conn->query($sql_insert_user);
-        ?>
-        <script>			
-                document.location.href="?lo";
-        </script>
-        <?php
+        $id_mou = $id_mou_baru;
+
+        $sql_insert_mou = "INSERT INTO `tb_mou` (`id_mou`,`institute_mou`,`contact_mou`) 
+                            VALUES ($id_mou, $nama_ip, $no_kontak)";
+        $conn->query($sql_insert_mou);
     }
+    $sql_insert_user = "INSERT INTO tb_user (id_user, username_user, password_user, name_user, email_user, level_user, create_user, id_mou, status_user) 
+                        VALUES (NULL, '$email', '$password', '$nama', '$email', '2', '$tanggal_sekarang', '$id_mou', 'Y')";
+    $conn->query($sql_insert_user);
+?>
+    <script>
+        document.location.href = "?lo";
+    </script>
+<?php
+}
 ?>
--- _user/index.php
+++ _user/index.php
@@ -1,188 +1,171 @@
 <?php
-	if(isset($_SESSION['id_user']) && isset($_SESSION['status_user'])=="Y")
-	{	
+if (isset($_SESSION['id_user']) && isset($_SESSION['status_user']) == "Y") {
 ?>
+
 	<body id="page-top">
-		
-	    <!-- Page Wrapper -->
-	    <div id="wrapper">
 
-	        <!-- Sidebar -->
-	        <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+		<!-- Page Wrapper -->
+		<div id="wrapper">
 
-	            <!-- Sidebar - Brand -->
-	            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="?">
-	                <div class="sidebar-brand-icon">
-	                    <i class="fas fa-laugh-wink"></i>
-	                </div>
-	                <div class="sidebar-brand-text mx-3">SIPAPAP MACO</div>
-	            </a>
+			<!-- Sidebar -->
+			<ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
-	            <!-- Divider -->
-	            <hr class="sidebar-divider my-0">
+				<!-- Sidebar - Brand -->
+				<a class="sidebar-brand d-flex align-items-center justify-content-center" href="?">
+					<div class="sidebar-brand-icon">
+						<i class="fas fa-laugh-wink"></i>
+					</div>
+					<div class="sidebar-brand-text mx-3">SIPAPAP MACO</div>
+				</a>
 
-	            <!-- Nav Item - Dashboard -->
-	            <li class="nav-item active">
-	                <a class="nav-link" href="?">
-	                    <i class="fas fa-fw fa-tachometer-alt"></i>
-	                    <span>Dashboard</span></a>
-	            </li>
+				<!-- Divider -->
+				<hr class="sidebar-divider my-0">
 
-	            <!-- Divider -->
-	            <hr class="sidebar-divider">
+				<!-- Nav Item - Dashboard -->
+				<li class="nav-item active">
+					<a class="nav-link" href="?">
+						<i class="fas fa-fw fa-tachometer-alt"></i>
+						<span>Dashboard</span></a>
+				</li>
 
-	            <!-- Heading -->
-	            <div class="sidebar-heading">
-	                Kediklatan
-	            </div>
+				<!-- Divider -->
+				<hr class="sidebar-divider">
 
-	            <!-- Nav Item - Pages Collapse Menu -->
-	            <li class="nav-item">
-	                <a class="nav-link" href="?prk">
-	                    <i class="fas fa-fw fa-chart-area"></i>
-	                    <span>Praktikan</span></a>
-	            </li>
-				
-				<?php 
-					if($_SESSION['level_user']==1)
-					{
-						?>
-						<li class="nav-item">
-							<a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities" aria-expanded="false" aria-controls="collapseUtilities">
-								<i class="fas fa-fw fa-table"></i>
-								<span>Data</span>
-							</a>
-							<div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities" data-parent="#accordionSidebar">
-								<div class="bg-white py-2 collapse-inner rounded">
-									<a class="collapse-item" href="?mou">MoU</a>
-									<a class="collapse-item" href="?pmb">Pembimbing</a>
-									<a class="collapse-item" href="?spf">Spesifikasi</a>
-								</div>
-							</div>
-						</li>
-						<?php
+				<!-- Heading -->
+				<div class="sidebar-heading">
+					Kediklatan
+				</div>
+
+				<!-- Nav Item - Pages Collapse Menu -->
+				<li class="nav-item">
+					<a class="nav-link" href="?prk">
+						<i class="fas fa-fw fa-chart-area"></i>
+						<span>Praktikan</span>
+					</a>
+				</li>
+
+				<!-- Divider -->
+				<hr class="sidebar-divider">
+
+				<!-- Heading -->
+				<div class="sidebar-heading">
+					Data
+				</div>
+				<?php
+				if ($_SESSION['level_user'] == 1) {
+				?>
+					<li class="nav-item">
+						<a class="nav-link" href="?prk">
+							<i class="fas fa-fw fa-table"></i>
+							<span>MoU</span>
+						</a>
+						<a class="nav-link" href="?prk">
+							<i class="fas fa-fw fa-table"></i>
+							<span>Pembimbing</span>
+						</a>
+						<a class="nav-link" href="?prk">
+							<i class="fas fa-fw fa-table"></i>
+							<span>Spesifikasi</span>
+						</a>
+					</li>
+				<?php
+				}
+				?>
+				<!-- Divider -->
+				<hr class="sidebar-divider d-none d-md-block">
+
+				<!-- Sidebar Toggler (Sidebar) -->
+				<div class="text-center d-none d-md-inline">
+					<button class="rounded-circle border-0" id="sidebarToggle"></button>
+				</div>
+			</ul>
+			<!-- End of Sidebar -->
+
+			<!-- Content Wrapper -->
+			<div id="content-wrapper" class="d-flex flex-column">
+
+				<!-- Main Content -->
+				<div id="content">
+					<?php
+					include "_nav_user.php";
+					if (isset($_GET['mou'])) {
+						include "_user/view/v_mou.php";
+					} elseif (isset($_GET['pmb'])) {
+						if (isset($_GET['i'])) {
+							include "_user/inset/i_pembimbing.php";
+						} elseif (isset($_GET['u'])) {
+							include "_user/update/u_pembimbing.php";
+						} else {
+							include "_user/view/v_pembimbing.php";
+						}
+					} elseif (isset($_GET['prk'])) {
+						if (isset($_GET['i'])) {
+							include "_user/insert/i_praktik.php";
+						} else {
+							include "_user/view/v_praktik.php";
+						}
+					} elseif (isset($_GET['spf'])) {
+						if (isset($_GET['u'])) {
+							include "_user/update/u_spesifikasi.php";
+						} elseif (isset($_GET['d'])) {
+							include "_user/delete/d_spesifikasi.php";
+						} elseif (isset($_GET['i'])) {
+							include "_user/insert/i_spesifikasi.php";
+						} else {
+							include "_user/view/v_spesifikasi.php";
+						}
 					}
-	            ?>
-	            <!-- Divider -->
-	            <hr class="sidebar-divider d-none d-md-block">
+					?>
+				</div>
+				<!-- End of Main Content -->
 
-	            <!-- Sidebar Toggler (Sidebar) -->
-	            <div class="text-center d-none d-md-inline">
-	                <button class="rounded-circle border-0" id="sidebarToggle"></button>
-	            </div>
-	        </ul>
-	        <!-- End of Sidebar -->
+				<!-- Footer -->
+				<footer class="sticky-footer bg-white">
+					<div class="container my-auto">
+						<div class="copyright text-center my-auto">
+							<span>RS Jiwa Provinsi Jawa Barat 2021</span>
+						</div>
+					</div>
+				</footer>
+				<!-- End of Footer -->
 
-	        <!-- Content Wrapper -->
-	        <div id="content-wrapper" class="d-flex flex-column">
+			</div>
+			<!-- End of Content Wrapper -->
 
-	            <!-- Main Content -->
-	            <div id="content">
-	                <?php
-	                    include "_nav_user.php";
-	                    if(isset($_GET['mou']))
-	                    {
-	                    	include "_user/view/v_mou.php";
-	                    }
-	                    elseif(isset($_GET['pmb']))
-	                    {
-	                    	if(isset($_GET['i']))
-	                    	{
-	                    		include "_user/inset/i_pembimbing.php";
-	                    	}
-	                    	elseif(isset($_GET['u']))
-	                    	{
-	                    		include "_user/update/u_pembimbing.php";
-	                    	}
-	                    	else
-	                    	{
-	                    		include "_user/view/v_pembimbing.php";
-	                    	}
-	                    }
-	                    elseif(isset($_GET['prk']))
-	                    {
-	                    	if(isset($_GET['i']))
-	                    	{
-	                    		include "_user/insert/i_praktik.php";
-	                    	}
-	                    	else
-	                    	{
-	                    		include "_user/view/v_praktik.php";
-	                    	}
-	                    }
-	                    elseif(isset($_GET['spf']))
-	                    {
-	                    	if(isset($_GET['u']))
-	                    	{
-	                    		include "_user/update/u_spesifikasi.php";
-	                    	}
-	                    	elseif(isset($_GET['d']))
-	                    	{
-	                    		include "_user/delete/d_spesifikasi.php";
-	                    	}
-	                    	elseif(isset($_GET['i']))
-	                    	{
-	                    		include "_user/insert/i_spesifikasi.php";
-	                    	}
-	                    	else
-	                    	{
-	                    		include "_user/view/v_spesifikasi.php";
-	                    	}
-	                    }
-	                ?>
-	            </div>
-	            <!-- End of Main Content -->
+		</div>
+		<!-- End of Page Wrapper -->
 
-	            <!-- Footer -->
-	            <footer class="sticky-footer bg-white">
-	                <div class="container my-auto">
-	                    <div class="copyright text-center my-auto">
-	                        <span>RS Jiwa Provinsi Jawa Barat 2021</span>
-	                    </div>
-	                </div>
-	            </footer>
-	            <!-- End of Footer -->
+		<!-- Scroll to Top Button-->
+		<a class="scroll-to-top rounded" href="#page-top">
+			<i class="fas fa-angle-up"></i>
+		</a>
 
-	        </div>
-	        <!-- End of Content Wrapper -->
-
-	    </div>
-	    <!-- End of Page Wrapper -->
-
-	    <!-- Scroll to Top Button-->
-	    <a class="scroll-to-top rounded" href="#page-top">
-	        <i class="fas fa-angle-up"></i>
-	    </a>
-
-	    <!-- Logout Modal-->
-	    <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
-	        aria-hidden="true">
-	        <div class="modal-dialog" role="document">
-	            <div class="modal-content">
-	                <div class="modal-header">
-	                    <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
-	                    <button class="close" type="button" data-dismiss="modal" aria-label="Close">
-	                        <span aria-hidden="true">×</span>
-	                    </button>
-	                </div>
-	                <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
-	                <div class="modal-footer">
-	                    <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-	                    <a class="btn btn-primary" href="?lo">Logout</a>
-	                </div>
-	            </div>
-	        </div>
-	    </div>
+		<!-- Logout Modal-->
+		<div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+			<div class="modal-dialog" role="document">
+				<div class="modal-content">
+					<div class="modal-header">
+						<h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
+						<button class="close" type="button" data-dismiss="modal" aria-label="Close">
+							<span aria-hidden="true">×</span>
+						</button>
+					</div>
+					<div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
+					<div class="modal-footer">
+						<button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+						<a class="btn btn-primary" href="?lo">Logout</a>
+					</div>
+				</div>
+			</div>
+		</div>
 	</body>
 <?php
-	}
-	else
-	{
-		?>
-		<script>
-			alert('Anda belum Login, Silahkan Login Terlebih dahulu');
-		</script>
-		<?php
-		header("Refresh:0; url=?ls");
-	}
-?> 
+} else {
+?>
+	<script>
+		alert('Anda belum Login, Silahkan Login Terlebih dahulu');
+	</script>
+<?php
+	header("Refresh:0; url=?ls");
+}
+?>
