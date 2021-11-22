fb4f2817813ce7a716e6765e6170a0362c08c071
unknown <rizkiegipurnama@gmail.com>
2021-11-22 11:44:52 +0700

    coba calender

--- index.php
+++ index.php
@@ -10,14 +10,16 @@ include "_header.php";
 if (isset($_SESSION['status_user']) == 'Y') {
     if (isset($_GET['lo'])) {
         include "_log-sign/log_out.php";
-    } elseif (isset($_SESSION['status_user']) == 'Y') {
+    }  elseif (isset($_SESSION['status_user']) == 'Y') {
         include "_user/index.php";
-    }
+    } 
 } elseif (empty($_SESSION['id_user']) || isset($_GET['ls'])) {
     if (isset($_GET['reg'])) {
         include "_log-sign/register.php";
     } elseif (isset($_GET['reg_x'])) {
         include "_log-sign/register_exc.php";
+    } else if (isset($_GET['coba'])) {
+        include "_log-sign/calendar.php";
     } else {
         include "_log-sign/index.php";
     }
