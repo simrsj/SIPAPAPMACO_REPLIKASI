305a6fdd03b0eee3204df4cc04324c4fa32212f0
Fajar PC <fajar.rachmat.h@gmail.com>
2021-12-24 07:16:45 +0700

    Proses Modal MoU

--- _admin/insert/i_praktik_mess.php
+++ _admin/insert/i_praktik_mess.php
@@ -1,3 +1,10 @@
+<?php
+$id_praktik = $_GET['m'];
+$q_praktik = $conn->query("SELECT * FROM tb_praktik WHERE id_praktik = $id_praktik");
+$d_praktik = $q_praktik->fetch(PDO::FETCH_ASSOC);
+$jumlah_praktik = $d_praktik['jumlah_praktik'];
+?>
+
 <div class="container-fluid">
     <div class="row">
         <div class="col-lg-8">
@@ -6,14 +13,14 @@
     </div>
     <div class="card shadow mb-4">
         <div class="card-body">
-            <form action="" method="POST" class="form-group">
-                <div class="row">
+            <form action="" method="POST" class="form-group ">
+                <div class="row d-flex align-items-center">
                     <div class="col-4">
                         Pilih Mess <span style="color:red">*</span><br>
                         <select class="form-control" name="id_mess" required>
                             <option value="">-- Pilih --</option>
                             <?php
-                            $q_jurusan = $conn->query("SELECT * FROM tb_mess order by nama_mess ASC");
+                            $q_jurusan = $conn->query("SELECT * FROM tb_mess WHERE ((kapasitas_t_mess - kapasitas_terisi_mess) >= $jumlah_praktik) ORDER BY nama_mess ASC");
                             while ($d_jurusan = $q_jurusan->fetch(PDO::FETCH_ASSOC)) {
                             ?>
                                 <option value="<?php echo $d_jurusan['id_mess']; ?>"><?php echo $d_jurusan['nama_mess']; ?></option>
@@ -22,17 +29,21 @@
                             ?>
                         </select>
                     </div>
-                    <div class="col-6">
+                    <div class="col-2">
                         Pilih Makan <span style="color:red">*</span><br>
                         <input type="radio" name="makan_mess" value="y" required> Tanpa Makan <br>
                         <input type="radio" name="makan_mess" value="t"> Dengan Makan (3x Sehari)
                     </div>
+                    <div class="col-2 ">
+                        <input class="btn btn-success" type="submit" name="SIMPAN" value="SIMPAN">
+                    </div>
                 </div>
                 <hr>
-                <input class="btn btn-success" type="submit" name="SIMPAN" value="SIMPAN">
             </form>
-
-            <hr>
+        </div>
+    </div>
+    <div class="card shadow mb-4">
+        <div class="card-body">
             <?php
             $sql_mess = "SELECT * FROM tb_mess WHERE status_mess = 'Aktif' ORDER BY nama_mess ASC";
             $q_mess = $conn->query($sql_mess);
