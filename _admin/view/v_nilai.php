0f8f18e059108b306a0578b0315352aae3963dda
Fajar (KANTOR) <fajar.rachmat.h@gmail.com>
2022-03-09 16:39:15 +0700

    proses data nilai upload

--- _admin/view/v_nilai.php
+++ _admin/view/v_nilai.php
@@ -46,8 +46,9 @@
                     $sql_data_praktikan .= " ORDER BY tb_praktikan.nama_praktikan ASC";
 
                     $q_data_praktikan = $conn->query($sql_data_praktikan);
+                    $q1_data_praktikan = $conn->query($sql_data_praktikan);
                     $r_data_praktikan = $q_data_praktikan->rowCount();
-                    $d1_data_praktikan = $q_data_praktikan->fetch(PDO::FETCH_ASSOC);
+                    $d1_data_praktikan = $q1_data_praktikan->fetch(PDO::FETCH_ASSOC);
                 ?>
                     <div id="accordion">
                         <div class="card">
@@ -104,62 +105,86 @@
                                     ?>
                                         <div class="table-responsive">
                                             <table class="table table-striped" id="myTable">
-                                                <?php
-                                                if ($d_praktik['id_jurusan_pdd'] == 2) {
-                                                ?>
-                                                    <thead class="thead-dark text-center">
-                                                        <tr>
-                                                            <th scope="col">No</th>
-                                                            <th scope="col">Nama Pembimbing </th>
-                                                            <th scope="col">NIP / NIPK</th>
-                                                            <th scope="col">Nama Ruangan</th>
+                                                <thead class="thead-dark text-center">
+                                                    <tr>
+                                                        <th scope="col">No</th>
+                                                        <th scope="col">Nama Pembimbing </th>
+                                                        <th scope="col">NIP / NIPK</th>
+                                                        <th scope="col">Nama Ruangan</th>
+                                                        <?php
+                                                        if ($d_praktik['id_jurusan_pdd'] == 2) {
+                                                        ?>
                                                             <th scope="col">Isi / Ubah <br>Nilai</th>
                                                             <th scope="col">Data Nilai</th>
-                                                        </tr>
-                                                    </thead>
-                                                    <tbody>
-                                                        <div id="accordion_nilai">
-                                                            <div class="card">
-                                                                <?php
-                                                                $total_jumlah_tarif = 0;
-                                                                $no = 1;
-                                                                while ($d_data_praktikan = $q_data_praktikan->fetch(PDO::FETCH_ASSOC)) {
-                                                                ?>
-                                                                    <tr>
-                                                                        <th scope="row"><?php echo $no; ?></th>
-                                                                        <td><?php echo $d_data_praktikan['nama_pembimbing']; ?></td>
-                                                                        <td><?php echo $d_data_praktikan['no_id_pembimbing']; ?></td>
-                                                                        <td><?php echo $d_data_praktikan['nama_unit']; ?></td>
-                                                                        <td class="text-center">
-                                                                            <?php
+                                                        <?php
+                                                        } else {
+                                                        ?>
+                                                            <th scope="col">File Nilai</th>
+                                                            <th scope="col">Unggah File Nilai</th>
+                                                        <?php
+                                                        }
+                                                        ?>
+                                                    </tr>
+                                                </thead>
+                                                <tbody>
+                                                    <div id="accordion_nilai">
+                                                        <div class="card">
+                                                            <?php
+                                                            $no = 1;
+                                                            while ($d_data_praktikan = $q_data_praktikan->fetch(PDO::FETCH_ASSOC)) {
+                                                            ?>
+                                                                <tr>
+                                                                    <th scope="row"><?php echo $no; ?></th>
+                                                                    <td><?php echo $d_data_praktikan['nama_pembimbing']; ?></td>
+                                                                    <td><?php echo $d_data_praktikan['no_id_pembimbing']; ?></td>
+                                                                    <td><?php echo $d_data_praktikan['nama_unit']; ?></td>
+                                                                    <td class="text-center">
+                                                                        <?php
 
-                                                                            $sql_data_nilai = "SELECT * FROM tb_nilai_kep ";
-                                                                            $sql_data_nilai .= " WHERE id_praktik = " . $d_data_praktikan['id_praktik'] . " AND id_pembimbing = " . $d_data_praktikan['id_pembimbing'];
-                                                                            // echo "$sql_data_nilai<br>";
+                                                                        $sql_data_nilai = "SELECT * FROM tb_nilai_kep ";
+                                                                        $sql_data_nilai .= " WHERE id_praktik = " . $d_data_praktikan['id_praktik'] . " AND id_pembimbing = " . $d_data_praktikan['id_pembimbing'];
+                                                                        // echo "$sql_data_nilai<br>";
 
-                                                                            $q_data_nilai = $conn->query($sql_data_nilai);
-                                                                            $r_data_nilai = $q_data_nilai->rowCount();
-                                                                            if ($r_data_nilai > 0) {
-                                                                            ?>
-                                                                                <a href="<?php echo "?nil&u=" . $d_praktik['id_praktik'] . "&p=" . $d_data_praktikan['id_pembimbing']; ?>" class="btn btn-outline-primary btn-sm">
-                                                                                    Ubah Nilai
-                                                                                </a>
-                                                                            <?php
-                                                                            } else {
-                                                                            ?>
-                                                                                <a href="<?php echo "?nil&i=" . $d_praktik['id_praktik'] . "&p=" . $d_data_praktikan['id_pembimbing']; ?>" class="btn btn-outline-success btn-sm">
-                                                                                    Isi Nilai
-                                                                                </a>
-                                                                            <?php
-                                                                            }
-                                                                            ?>
-                                                                        </td>
+                                                                        $q_data_nilai = $conn->query($sql_data_nilai);
+                                                                        $r_data_nilai = $q_data_nilai->rowCount();
+                                                                        if ($r_data_nilai > 0) {
+                                                                        ?>
+                                                                            <a href="<?php echo "?nil&u=" . $d_praktik['id_praktik'] . "&p=" . $d_data_praktikan['id_pembimbing']; ?>" class="btn btn-outline-primary btn-sm">
+                                                                                Ubah Nilai
+                                                                            </a>
+                                                                        <?php
+                                                                        } else {
+                                                                        ?>
+                                                                            <a href="<?php echo "?nil&i=" . $d_praktik['id_praktik'] . "&p=" . $d_data_praktikan['id_pembimbing']; ?>" class="btn btn-outline-success btn-sm">
+                                                                                Isi Nilai
+                                                                            </a>
+                                                                        <?php
+                                                                        }
+                                                                        ?>
+                                                                    </td>
+                                                                    <?php
+                                                                    if ($d_praktik['id_jurusan_pdd'] == 2) {
+                                                                    ?>
                                                                         <td class="text-center">
-                                                                            <button class="btn btn-info btn-sm collapsed" data-toggle="collapse" data-target="#nilai<?php echo $d_praktik['id_praktik']; ?>" title="Rincian">
+                                                                            <button class="btn btn-info btn-sm collapsed" data-toggle="collapse" data-target="#nilai<?php echo $no; ?>" title="Rincian">
                                                                                 <i class="fas fa-info-circle"></i> Rincian Nilai
                                                                             </button>
                                                                         </td>
-                                                                    </tr>
+                                                                    <?php
+                                                                    } else {
+                                                                    ?>
+                                                                        <td class="text-center">
+                                                                            <button class="btn btn-info btn-sm collapsed" data-toggle="collapse" data-target="#nilai<?php echo $no; ?>" title="Rincian">
+                                                                                <i class="fas fa-info-circle"></i> Rincian Nilai
+                                                                            </button>
+                                                                        </td>
+                                                                    <?php
+                                                                    }
+                                                                    ?>
+                                                                </tr>
+                                                                <?php
+                                                                if ($d_praktik['id_jurusan_pdd'] == 2) {
+                                                                ?>
                                                                     <tr>
                                                                         <td colspan="6">
                                                                             <div id="nilai<?php echo $no; ?>" class="collapse text-center" aria-labelledby="nilai<?php echo $no; ?>" data-parent="#accordion_nilai">
@@ -244,47 +269,17 @@
                                                                             </div>
                                                                         </td>
                                                                     </tr>
+
                                                                 <?php
-                                                                    $no++;
                                                                 }
                                                                 ?>
-                                                            </div>
+                                                            <?php
+                                                                $no++;
+                                                            }
+                                                            ?>
                                                         </div>
-                                                    </tbody>
-
-                                                <?php
-                                                } else {
-                                                ?>
-                                                    <thead class="thead-dark text-center">
-                                                        <tr>
-                                                            <th scope="col">No</th>
-                                                            <th scope="col">Nama Pembimbing </th>
-                                                            <th scope="col">NIP / NIPK</th>
-                                                            <th scope="col">Nama Ruangan</th>
-                                                            <th scope="col">Data Nilai</th>
-                                                        </tr>
-                                                    </thead>
-                                                    <tbody>
-                                                        <?php
-                                                        $total_jumlah_tarif = 0;
-                                                        $no = 1;
-                                                        while ($d_data_praktikan = $q_data_praktikan->fetch(PDO::FETCH_ASSOC)) {
-                                                        ?>
-                                                            <tr>
-                                                                <th scope="row"><?php echo $no; ?></th>
-                                                                <td><?php echo $d_data_praktikan['nama_pembimbing']; ?></td>
-                                                                <td><?php echo $d_data_praktikan['nama_unit']; ?></td>
-                                                                <td>Status</td>
-                                                                <td><?php echo $d_data_praktikan['no_id_praktikan']; ?></td>
-                                                            </tr>
-                                                        <?php
-                                                            $no++;
-                                                        }
-                                                        ?>
-                                                    </tbody>
-                                                <?php
-                                                }
-                                                ?>
+                                                    </div>
+                                                </tbody>
                                             </table>
                                         </div>
                                     <?php
