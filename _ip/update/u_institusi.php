cde162f91440684648a312fa0565e8abe51f238b
fajarrachmath <81356042+fajarrachmath@users.noreply.github.com>
2022-03-16 06:52:23 +0700

    perbaikan data institusi

--- _ip/update/u_institusi.php
+++ _ip/update/u_institusi.php
@@ -19,16 +19,18 @@
                         ?>
                         Institusi : <br>
                         <b> <?php echo $d['nama_institusi'] ?></b>
+                        <input type="hidden" name="nama_institusi" value="<?php echo $d['nama_institusi'] ?>">
+                        <input type="hidden" name="id_institusi" value="<?php echo $d['id_institusi'] ?>">
                         <br><br>
                         Akronim : <br>
                         <input type="text" class="form-control" name="akronim_institusi" value="<?php echo $d['akronim_institusi'] ?>">
                         <br>
                         Logo : <br>
-                        <input type="file" name="logo_institusi" accept="image/png"><br>
+                        <input type="file" name="logo_institusi" id="logo_institusi" accept="image/png"><br>
                         <span class="text-xs font-italic">Format File PNG, ukuran file dibawah 500 Kb</span>
                         <br><br>
                         Alamat : <br>
-                        <textarea class="form-control" name="akronim_institusi"><?php echo $d['alamat_institusi'] ?></textarea>
+                        <textarea class="form-control" name="alamat_institusi"><?php echo $d['alamat_institusi'] ?></textarea>
                         <br>
                         Akreditasi : <span class="text-danger">*</span><br>
                         <?php
@@ -59,10 +61,122 @@
                         <br><br>
                         <span class="font-weight-bold font-italic"><span class=" text-danger">*</span> : Wajib Diisi</span>
                         <hr>
-                        <center><button class="btn btn-success btn-sm" type="submit" name="simpan">SIMPAN DATA</button></center>
+                        <center><button class="btn btn-success btn-sm" type="submit" name="simpan" onclick="uploadFile();">SIMPAN DATA</button></center>
                     </form>
                 </div>
             </div>
         </div>
     </div>
 </div>
+
+
+<script>
+    // Upload file
+    function uploadFile() {
+
+        //Cari ekstensi file yg diupload
+        var fileName = document.querySelector('#logo_institusi').value;
+        var type = fileName.split('.').pop();
+
+        //cari ukuran file yg diupload
+        var size = document.getElementById("file").files[0].size / 1024;
+
+        //Toast bila upload file selain pdf
+        if (type != 'pdf') {
+            const Toast = Swal.mixin({
+                toast: true,
+                position: 'top-end',
+                showConfirmButton: false,
+                timer: 10000,
+                timerProgressBar: true,
+                didOpen: (toast) => {
+                    toast.addEventListener('mouseenter', Swal.stopTimer)
+                    toast.addEventListener('mouseleave', Swal.resumeTimer)
+                }
+            });
+
+            Toast.fire({
+                icon: 'warning',
+                title: '<div class="text-md text-center">File Surat Harus <b>.pdf</b></div>'
+            });
+        } else if (size > 1024) {
+            const Toast = Swal.mixin({
+                toast: true,
+                position: 'top-end',
+                showConfirmButton: false,
+                timer: 10000,
+                timerProgressBar: true,
+                didOpen: (toast) => {
+                    toast.addEventListener('mouseenter', Swal.stopTimer)
+                    toast.addEventListener('mouseleave', Swal.resumeTimer)
+                }
+            });
+
+            Toast.fire({
+                icon: 'warning',
+                title: '<div class="text-md text-center">File Surat Harus <br><b>Kurang dari 1 Mb</b></div>'
+            });
+        } else if (
+            files.length > 0 &&
+            type == 'pdf'
+        ) {
+
+            var formData = new FormData();
+            formData.append("file", files[0]);
+
+            var xhttp = new XMLHttpRequest();
+            xhttp.open("POST", "test1.php", true);
+            xhttp.onreadystatechange = function() {
+            };
+            xhttp.send(formData);
+
+        }
+
+    }
+</script>
+<?php
+if (isset($_POST['simpan'])) {
+
+
+    if ($_FILES['logo_institusi']['size'] > 0) {
+        $alamat_unggah = "./_img/logo_institusi/temp/";
+
+        if (!is_dir($alamat_unggah)) {
+            mkdir($alamat_unggah, 0777, $rekursif = true);
+        }
+        $_FILES['logo_institusi']['name'] = $_POST['id'] . ".png";
+
+        if(){
+        }elseif (!is_null($_FILES['logo_institusi'])) {
+            $logo_institusi = (object) @$_FILES['logo_institusi'];
+
+            $unggah_logo_institusi = move_uploaded_file(
+                $logo_institusi->tmp_name,
+                "{$alamat_unggah}/{$logo_institusi->name}"
+            );
+            $link_logo_institusi = "{$alamat_unggah}/{$logo_institusi->name}";
+        }
+    }
+
+    $sql = "INSERT INTO tb_institusi_temp
+    (
+        nama_insTemp, 
+        akronim_insTemp, 
+        logo_insTemp, 
+        alamat_insTemp, 
+        akr_insTemp, 
+        tglAkr_insTemp, 
+        fileAkr_insTemp
+    ) VALUE (
+        '" . $_POST['nama_institusi'] . "',
+        '" . $_POST['akronim_institusi'] . "',
+        '" . $_POST['logo_institusi'] . "',
+        '" . $_POST['alamat_institusi'] . "',
+        '" . $_POST['akred_institusi'] . "',
+        '" . $_POST['tglAkhirAkred_institusi'] . "',
+        '" . $_POST['fileAkred_institusi'] . "'
+    
+    ";
+}
+
+?>
--- _ip/view/v_institusi.php
+++ _ip/view/v_institusi.php
@@ -9,8 +9,13 @@
     </div>
 
     <div class="row">
-        <div class="col-lg">
+
+        <div class="col-xl-3 col-md-3 mb-4  align-items-stretch justify-content-center">
+
             <div class="card shadow mb-4">
+                <div class="card-header flex-row align-items-center justify-content-between">
+                    <h6 class="m-0 font-weight-bold text-primary text-center">Data Profil Institusi</h6>
+                </div>
                 <div class="card-body text-center">
                     <?php
                     $sql = "SELECT * FROM tb_institusi ";
@@ -118,6 +123,157 @@
                         }
                         ?>
                     </b><br><br>
+                </div>
+            </div>
+        </div>
+
+        <!-- DATA KEPERAWATAN -->
+        <div class="col-xl-3 col-md-3 mb-4  align-items-stretch justify-content-center">
+            <div class="card  shadow h-100">
+                <div class="card-header   flex-row align-items-center justify-content-between">
+                    <h6 class="m-0 font-weight-bold text-primary">Keperawatan</h6>
+                </div>
+                <div class="card-body">
+                    <div>
+                        <div class="no-gutters align-items-center">
+                            Jurusan : Keperawatan
+                            <hr>
+                            Jenjang : D3, S1, Profesi.
+                            <hr>
+                            Profesi : NERS
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-xl-6 col-md-6 mb-4 align-items-stretch">
+        </div>
+        <div class="col-xl-6 col-md-6 mb-4 align-items-stretch">
+            <div class="card shadow mb-4">
+                <div class="card-header flex-row align-items-center justify-content-between">
+                    <h6 class="m-0 font-weight-bold text-primary text-center">Data Pengajuan Profil Institusi</h6>
+                </div>
+                <div class="card-body text-center">
+                    <?php
+                    $sql = "SELECT * FROM tb_institusi_temp ";
+                    $sql .= " WHERE id_institusi = " . $_SESSION['id_institusi'];
+                    $q = $conn->query($sql);
+
+                    $d = $q->fetch(PDO::FETCH_ASSOC);
+                    if ($q->rowCount() > 0) {
+                    ?>
+                        Institusi : <br>
+                        <b><?php echo $d['nama_institusi']; ?></b><br><br>
+                        Akronim :<br>
+                        <b>
+                            <?php
+                            if ($d['akronim_institusi'] == "") {
+                            ?>
+                                <span class="badge badge-danger">Data Tidak Ada</span>
+                            <?php
+                            } else {
+                                echo $d['akronim_institusi'];
+                            }
+                            ?>
+                        </b><br><br>
+                        Logo :<br>
+                        <b>
+                            <?php
+                            if ($d['logo_institusi'] == "") {
+                            ?>
+                                <span class="badge badge-danger">Data Tidak Ada</span>
+                            <?php
+                            } else {
+                            ?>
+                                <a title="Lihat Logo" class='btn btn-info btn-sm' href='#' data-toggle='modal' data-target='<?php echo "#see_" . $d['id_institusi']; ?>'>
+                                    <i class="fas fa-eye"></i> Lihat
+                                </a>
+
+                                <!-- Lihat Logo  -->
+                                <div class="modal fade text-center" id="<?php echo "see_" . $d['id_institusi']; ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                    <div class="modal-dialog" role="document">
+                                        <div class="modal-content">
+                                            <div class="modal-body">
+                                                <img src="<?php echo $d['logo_institusi']; ?>" width="250px" height="250px">
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            <?php
+                            }
+                            ?>
+                        </b>
+                        <br><br>
+                        Alamat :<br>
+                        <b>
+                            <?php
+                            if ($d['alamat_institusi'] == "") {
+                            ?>
+                                <span class="badge badge-danger">Data Tidak Ada</span>
+                            <?php
+                            } else {
+                                echo $d['alamat_institusi'];
+                            }
+                            ?>
+                        </b><br><br>
+                        Akreditasi :<br>
+                        <b>
+                            <?php
+                            if ($d['akred_institusi'] == "") {
+                            ?>
+                                <span class="badge badge-danger">Data Tidak Ada</span>
+                            <?php
+                            } else {
+                                echo $d['alamat_institusi'];
+                            }
+                            ?>
+                        </b><br><br>
+                        Tanggal Akreditasi Berlaku:<br>
+                        <b>
+                            <?php
+                            if ($d['tglAkhirAkred_institusi'] == "") {
+                            ?>
+                                <span class="badge badge-danger">Data Tidak Ada</span>
+                                <?php
+                            } else {
+                                if ($d['tglAkhirAkred_institusi'] >= date('Y-m-d')) {
+                                ?>
+                                    <span class="badge badge-danger">Sudah Tidak Berlaku</span><br>
+                            <?php
+                                }
+                                echo tanggal($d['tglAkhirAkred_institusi']);
+                            }
+                            ?>
+                        </b><br><br>
+                        File Akreditasi :<br>
+                        <b>
+                            <?php
+                            if ($d['fileAkred_institusi'] == "") {
+                            ?>
+                                <span class="badge badge-danger">Data Tidak Ada</span>
+                            <?php
+                            } else {
+                            ?>
+                                <a href='<?php echo $d['fileAkred_institusi']; ?>' target=" _blank" class="btn btn-success btn-sm">
+                                    <i class="fas fa-file-download"></i> Download
+                                </a>
+                            <?php
+                            }
+                            ?>
+                        </b><br><br>
+                    <?php
+                    } else {
+                    ?>
+                        <div class="jumbotron">
+                            <div class="jumbotron-fluid">
+                                <div class="text-gray-700" style="padding-bottom: 2px; padding-top: 5px;">
+                                    <h5 class="text-center">DATA PENGAJUAN PROFIL INSTITUSI TIDAK ADA</h5>
+                                </div>
+                            </div>
+                        </div>
+                    <?php
+                    }
+                    ?>
                 </div>
             </div>
         </div>
