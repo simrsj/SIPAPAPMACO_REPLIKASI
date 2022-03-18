49eba22037b226ad250684738baa3359b6a2c782
Fajar (KANTOR) <fajar.rachmat.h@gmail.com>
2022-03-18 15:26:57 +0700

    perbaikan val profil institusi

--- test.php
+++ test.php
@@ -1,3 +1,1 @@
-<?php
-copy("./_img/logo_institusi/temp/index1.php", "./_img/logo_institusi/index1.php");
-unlink("./_img/logo_institusi/temp/index1.php");
+<a href="./_print/p_praktikInv.php" class="btn btn-primary btn-xs" target="_blank">invoice</a>
