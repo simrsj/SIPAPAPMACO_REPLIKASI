2ac83ce880e116c59b42102a2d5ac6e975bfec39
rizkiegipurnama <rizkiegipurnama@gmail.com>
2022-03-28 14:39:53 +0700

    dashboard mou ip

--- _add-ons/dashboard_data_ip.php
+++ _add-ons/dashboard_data_ip.php
@@ -4,7 +4,9 @@ $id_ins = $_SESSION['id_institusi'];
 
 //////////////////// DATA INSTITUSi ////////////////////
 $sql_ins = " SELECT * FROM tb_institusi ";
-$sql_ins .= " WHERE id_institusi = " . $id_ins;
+$sql_ins .= "JOIN tb_user on tb_institusi.id_institusi = tb_user.id_institusi ";
+$sql_ins .= "JOIN tb_mou on tb_institusi.id_institusi = tb_mou.id_institusi ";
+$sql_ins .= " WHERE tb_institusi.id_institusi = " . $id_ins;
 $q_ins = $conn->query($sql_ins);
 
 $d_ins = $q_ins->fetch(PDO::FETCH_ASSOC);
@@ -16,5 +18,6 @@ $dAr_ins['akred_institusi'] = $d_ins['akred_institusi'];
 $dAr_ins['tglAkhirAkred_institusi'] = $d_ins['tglAkhirAkred_institusi'];
 $dAr_ins['fileAkred_institusi'] = $d_ins['fileAkred_institusi'];
 $dAr_ins['ket_institusi'] = $d_ins['ket_institusi'];
+$dAr_ins['tgl_selesai_mou'] = $d_ins['tgl_selesai_mou'];
 
 //////////////////// DATA INSTITUSi ////////////////////
