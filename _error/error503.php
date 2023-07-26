fc54f33e0219858e6e7a5ad40063699222692e32
fajarrachmath <fajar.rachmat.h@gmail.com>
2023-07-26 10:05:32 +0700

    Perbaikan erro 503

--- _error/error503.php
+++ _error/error503.php
@@ -1,12 +1,1 @@
-<div class=" card ">
-    <div class="card-body page-wrap d-flex flex-row align-items-center">
-        <div class="container">
-            <div class="row justify-content-center">
-                <div class="col-md-12 text-center">
-                    <span class="display-1 d-block">503</span>
-                    <div class="mb-4 lead">Service Unavailable</div>
-                    <a href="./" class="btn btn-outline-primary">Kembali ke Halaman Utama</a>
-                </div>
-            </div>
-        </div>
-    </div>
+<center>503: Service Unavailable</center>
