2428abeca3a9963192fd9496157ce225c7dca1bf
fajarrachmath <fajar.rachmat.h@gmail.com>
2023-10-31 10:40:25 +0700

    pembuatan ubah e-log book residen PKD

--- _admin/exc/x_v_ked_residen_pkd_data_u.php
+++ _admin/exc/x_v_ked_residen_pkd_data_u.php
@@ -5,30 +5,25 @@ include $_SERVER['DOCUMENT_ROOT'] . "/SM/_add-ons/crypt.php";
 // print_r($_POST);
 // echo "</pre>";
 error_reporting(0);
-$i = 0;
 try {
-    $sql = "UPDATE tb_logbook_ked_residen_jkh SET ";
+    $sql = "UPDATE tb_logbook_ked_residen_pkd SET ";
     $sql .= " tgl_ubah = '" . date('Y-m-d G:i:s') . "',";
+    $sql .= " jenis = '" . $_POST['jenis'] . "',";
     $sql .= " tgl = '" . $_POST['tgl'] . "',";
-    $sql .= " visite_besar = '" . $_POST['visite_besar'] . "',";
-    $sql .= " rapat_klinik = '" . $_POST['rapat_klinik'] . "',";
-    $sql .= " acara_ilmiah = '" . $_POST['acara_ilmiah'] . "',";
-    $sql .= " matkul_dosen = '" . $_POST['matkul_dosen'] . "',";
-    $sql .= " j_pasien_rajal = '" . $_POST['j_pasien_rajal'] . "',";
-    $sql .= " j_pasien_rajal = '" . $_POST['j_pasien_rajal'] . "'";
+    $sql .= " semester = '" . $_POST['semester'] . "',";
+    $sql .= " no_rm = '" . $_POST['no_rm'] . "',";
+    $sql .= " inisial = '" . $_POST['inisial'] . "',";
+    $sql .= " icd10_diagnosis = '" . $_POST['icd10_diagnosis'] . "',";
+    $sql .= " ket = '" . $_POST['ket'] . "'";
     $sql .= " WHERE id = " . decryptString($_POST['id'], $customkey);
     $conn->query($sql);
     echo json_encode([
-        // 'sql' => $sql,
-        // 'cok' => $i,
+        'sql' => $sql,
         'ket' => 'SUCCESS'
     ]);
-    $i++;
 } catch (PDOException $ex) {
     echo json_encode([
-        // 'sql' => $sql,
-        // 'cek' => 'cek',        
+        'sql' => $sql,
         'ket' => $ex->getMessage() . $ex->getLine()
-
     ]);
 }
